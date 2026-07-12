2f30afe1934f046c0b7f3c95fa0940bd3f44fe94
Kakul Mittal <kakulmittal2006@gmail.com>
2026-07-12 14:44:14 +0530

    Implement fuel controller with CRUD operations

---
+++ backend/controllers/fuelController.js
@@ -0,0 +1,22 @@
+const Fuel = require('../models/Fuel');
+const { ApiError, asyncHandler } = require('../middlewares/errorHandler');
+
+const getFuelById = async (id) => {
+  const fuel = await Fuel.findById(id);
+  if (!fuel) throw new ApiError(404, 'Fuel log not found.');
+  return fuel;
+};
+
+exports.createFuel = asyncHandler(async (req, res) => res.status(201).json(await Fuel.create(req.body)));
+exports.getFuels = asyncHandler(async (req, res) => res.json(await Fuel.find().sort({ date: -1 })));
+exports.getFuel = asyncHandler(async (req, res) => res.json(await getFuelById(req.params.id)));
+exports.updateFuel = asyncHandler(async (req, res) => {
+  const fuel = await Fuel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+  if (!fuel) throw new ApiError(404, 'Fuel log not found.');
+  res.json(fuel);
+});
+exports.deleteFuel = asyncHandler(async (req, res) => {
+  const fuel = await Fuel.findByIdAndDelete(req.params.id);
+  if (!fuel) throw new ApiError(404, 'Fuel log not found.');
+  res.status(204).send();
+});
